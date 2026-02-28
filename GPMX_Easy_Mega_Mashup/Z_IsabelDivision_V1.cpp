aa13dc14a5cb08f99e5fe26f99045be4ed1978b5
MaricarmenBP <maricarmenbuenfil004@gmail.com>
2026-02-27 19:16:56 -0600

    Problem 26

--- GPMX_Easy_Mega_Mashup/Z_IsabelDivision_V1.cpp
+++ GPMX_Easy_Mega_Mashup/Z_IsabelDivision_V1.cpp
@@ -4,14 +4,21 @@ using namespace std;
 using ll = long long;
 
 void solve() {
+   int n; cin>>n;
+   int num = n, count = 0;
+
+   while( n ){
+    if( n%10 != 0 && num%(n%10) == 0  ) count++;
+    n /= 10;
+   }
+
+   cout<<count<<"\n";
    
 }
 
 int main() {
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    //ll t; cin>>t;
     
-    //while(t--)
     solve();
     
     return 0;
