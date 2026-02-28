7acfc3c29349a8b576a453712c8016fc8df17975
MaricarmenBP <maricarmenbuenfil004@gmail.com>
2026-02-27 19:17:27 -0600

    Problem 27

--- GPMX_Easy_Mega_Mashup/ZA_HogFencing_V1.cpp
+++ GPMX_Easy_Mega_Mashup/ZA_HogFencing_V1.cpp
@@ -4,14 +4,13 @@ using namespace std;
 using ll = long long;
 
 void solve() {
-   
+   int n; cin>>n;
+   cout << (n/4)*((n - (n/4)*2)/2)<<"\n";
 }
 
 int main() {
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    //ll t; cin>>t;
-    
-    //while(t--)
+
     solve();
     
     return 0;
